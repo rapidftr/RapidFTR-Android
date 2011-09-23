14b7aec924fc0633a13e90f6c652f8170d00833a
Radu Muresan <cavey79@gmail.com>
2011-09-23 15:30:29 -0600

    radu - First pass at implementing the login page (connecting to the service and just displaying a popup message for now indicating whether login was successful or not).

---
+++ RapidFTR-Android/src/com/rapidftr/activity/LoginActivity.java
@@ -0,0 +1,91 @@
+package com.rapidftr.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.rapidftr.CameraPreviewActivity;
+import com.rapidftr.R;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Radu Muresan
+ * Date: 9/23/11
+ * Time: 1:17 PM
+ */
+public class LoginActivity extends Activity {
+
+    public static final String DEFAULT_USERNAME = "rapidftr";
+    public static final String DEFAULT_PASSWORD = "rapidftr";
+    public static final String DEFAULT_URL = "dev.rapidftr.com:3000";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        ((EditText) findViewById(R.id.login_url)).setHint(DEFAULT_URL);
+        ((EditText) findViewById(R.id.username)).setHint(DEFAULT_USERNAME);
+        ((EditText) findViewById(R.id.password)).setHint(DEFAULT_PASSWORD);
+        findViewById(R.id.login_button).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String loginUrl = getEditText(R.id.login_url, DEFAULT_URL);
+                String username = getEditText(R.id.username, DEFAULT_USERNAME);
+                String password = getEditText(R.id.password, DEFAULT_PASSWORD);
+                try {
+                    login(loginUrl, username, password);
+                } catch (IOException e) {
+                    displayMessage("Login Failed: " + e.getMessage());
+                }
+            }
+        });
+
+        //TODO temporary camera spike code
+        findViewById(R.id.camera_button).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, CameraPreviewActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    //TODO move this out to a service layer
+    private void login(String loginUrl, String username, String password) throws IOException {
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost post = new HttpPost("http://" + loginUrl + "/sessions");
+        post.addHeader("Accept", "application/json");
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+        nameValuePairs.add(new BasicNameValuePair("user_name", username));
+        nameValuePairs.add(new BasicNameValuePair("password", password));
+        nameValuePairs.add(new BasicNameValuePair("imei", "123456"));
+        nameValuePairs.add(new BasicNameValuePair("mobile_number", "155512345678"));
+        post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+        HttpResponse response = httpClient.execute(post);
+        String message = response.getStatusLine().getStatusCode() == 201
+                ? "Login Successful" : "Login Failed: " + response.getStatusLine().toString();
+        displayMessage(message);
+    }
+
+    private void displayMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    private String getEditText(int resId, String defaultValue) {
+        String currentValue = ((EditText) findViewById(resId)).getText().toString().trim();
+        return currentValue.equals("") ? defaultValue : currentValue;
+    }
+
+}
