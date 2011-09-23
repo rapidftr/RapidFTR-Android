ccf471c774f8d5677373c2d7ae49afcbb73d16ce
Caue Guerra <caueguerra@gmail.com>
2011-09-23 14:43:53 -0600

    adding camera spike. still need to add tests and refactor, but I think it is working

---
+++ RapidFTR-Android/src/com/rapidftr/CameraPreviewActivity.java
@@ -0,0 +1,73 @@
+package com.rapidftr;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.*;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class CameraPreviewActivity extends Activity {
+    private static final String TAG = "CameraDemo";
+
+    private Preview preview;
+    private Button buttonClick;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camera);
+
+        preview = new Preview(this);
+        ((FrameLayout) findViewById(R.id.preview)).addView(preview);
+
+        buttonClick = (Button) findViewById(R.id.buttonClick);
+        buttonClick.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                preview.getCamera().takePicture(null, null, jpegCallback);
+            }
+        });
+
+        Log.d(TAG, "onCreate'd");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        preview.openCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        preview.releaseCamera();
+    }
+
+	Camera.PictureCallback jpegCallback = new Camera.PictureCallback() {
+		public void onPictureTaken(byte[] data, Camera camera) {
+			FileOutputStream outStream = null;
+			try {
+				// write to local sandbox file system
+				// outStream =
+				// CameraDemo.this.openFileOutput(String.format("%d.jpg",
+				// System.currentTimeMillis()), 0);
+				// Or write to sdcard
+				outStream = new FileOutputStream(String.format(
+						"/sdcard/%d.jpg", System.currentTimeMillis()));
+				outStream.write(data);
+				outStream.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+            Log.d(TAG, "onPictureTaken - jpeg");
+		}
+	};
+}
---
+++ RapidFTR-Android/src/com/rapidftr/Preview.java
@@ -0,0 +1,102 @@
+package com.rapidftr;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static android.view.SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS;
+
+public class Preview extends SurfaceView implements SurfaceHolder.Callback {
+    private static final String TAG = "Preview";
+
+    SurfaceHolder mHolder;
+    private Camera camera;
+
+    Preview(Context context) {
+        super(context);
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    public Camera getCamera() {
+        return camera;
+    }
+
+    public void openCamera() {
+        if (camera == null) {
+            camera = Camera.open();
+        }
+    }
+
+    public void releaseCamera() {
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, acquire the camera and tell it where to draw.
+        openCamera();
+        try {
+            camera.setPreviewDisplay(holder);
+
+            camera.setPreviewCallback(new Camera.PreviewCallback() {
+
+                public void onPreviewFrame(byte[] data, Camera arg1) {
+                    FileOutputStream outStream;
+                    try {
+                        outStream = new FileOutputStream(String.format(
+                                "/sdcard/%d.jpg", System.currentTimeMillis()));
+                        outStream.write(data);
+                        outStream.close();
+                        Log.d(TAG, "onPreviewFrame - wrote bytes: " + data.length);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Preview.this.invalidate();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // Surface will be destroyed when we return, so stop the preview.
+        // Because the CameraDevice object is not a shared resource, it's very
+        // important to release it when the activity is paused.
+        camera.stopPreview();
+        camera = null;
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // Now that the size is known, set up the camera parameters and begin the preview.
+        Camera.Parameters parameters = camera.getParameters();
+        parameters.setPreviewSize(w, h);
+        camera.setParameters(parameters);
+        camera.startPreview();
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        Paint p = new Paint(Color.RED);
+        Log.d(TAG, "draw");
+        canvas.drawText("PREVIEW", canvas.getWidth() / 2, canvas.getHeight() / 2, p);
+    }
+}
