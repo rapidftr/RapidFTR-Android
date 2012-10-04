cbf553b4fe8c814517f59691b35146440cfa6912
Yekkanti Kishore Kumar <kishoreyekkanti@gmail.com>
2012-10-04 11:31:54 +0530

    styles for android

--- RapidFTR-Android/target/generated-sources/r/com/rapidftr/R.java
+++ RapidFTR-Android/target/generated-sources/r/com/rapidftr/R.java
@@ -16,25 +16,34 @@ public final class R {
     }
     public static final class drawable {
         public static final int bg_android=0x7f020000;
-        public static final int icon=0x7f020001;
-        public static final int logo=0x7f020002;
+        public static final int bg_btn=0x7f020001;
+        public static final int button=0x7f020002;
+        public static final int button_focused=0x7f020003;
+        public static final int button_normal=0x7f020004;
+        public static final int button_pressed=0x7f020005;
+        public static final int ic_action_search=0x7f020006;
+        public static final int icon=0x7f020007;
+        public static final int logo=0x7f020008;
+        public static final int textbox=0x7f020009;
+        public static final int textbox_focused=0x7f02000a;
+        public static final int textbox_normal=0x7f02000b;
     }
     public static final class id {
-        public static final int base_url=0x7f060005;
-        public static final int buttonClick=0x7f060002;
-        public static final int change_url=0x7f060007;
-        public static final int details=0x7f06000a;
-        public static final int field_options=0x7f06000d;
-        public static final int label=0x7f06000c;
-        public static final int layout=0x7f060000;
-        public static final int login_button=0x7f060006;
-        public static final int password=0x7f060004;
-        public static final int preview=0x7f060001;
-        public static final int register_child_button=0x7f060008;
-        public static final int spinner=0x7f060009;
-        public static final int submit=0x7f06000b;
-        public static final int username=0x7f060003;
-        public static final int value=0x7f06000e;
+        public static final int base_url=0x7f070005;
+        public static final int buttonClick=0x7f070002;
+        public static final int change_url=0x7f070007;
+        public static final int details=0x7f07000a;
+        public static final int field_options=0x7f07000d;
+        public static final int label=0x7f07000c;
+        public static final int layout=0x7f070000;
+        public static final int login_button=0x7f070006;
+        public static final int password=0x7f070004;
+        public static final int preview=0x7f070001;
+        public static final int register_child_button=0x7f070008;
+        public static final int spinner=0x7f070009;
+        public static final int submit=0x7f07000b;
+        public static final int username=0x7f070003;
+        public static final int value=0x7f07000e;
     }
     public static final class layout {
         public static final int audio_upload_box=0x7f030000;
@@ -68,5 +77,10 @@ public final class R {
         public static final int url=0x7f04000b;
         public static final int username=0x7f040009;
         public static final int view_all_children=0x7f040006;
+    }
+    public static final class style {
+        public static final int AppTheme=0x7f060000;
+        public static final int button=0x7f060001;
+        public static final int links=0x7f060002;
     }
 }
