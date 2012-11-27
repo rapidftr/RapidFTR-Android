3752240c489172431de1523983dc96f2b2bacf5f
Yekkanti Kishore Kumar <kishoreyekkanti@gmail.com>
2012-11-27 16:18:56 +0530

    [Integration Tests] Yekkanti - Integration tests across the web app and the mobile. Children data could be deleted for every test run if needed.

--- RapidFTR-AndroidTests/src/main/java/com/rapidftr/activity/DataSyncingIntegrationTest.java
+++ RapidFTR-AndroidTests/src/main/java/com/rapidftr/activity/DataSyncingIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.rapidftr.activity;
 
+import com.rapidftr.R;
 import com.rapidftr.RapidFtrApplication;
 import com.rapidftr.model.Child;
 import com.rapidftr.repository.ChildRepository;
@@ -9,26 +10,38 @@ import java.io.IOException;
 
 import static com.rapidftr.utils.RapidFtrDateTime.now;
 import static com.rapidftr.utils.http.FluentRequest.http;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
 
 public class DataSyncingIntegrationTest extends BaseActivityIntegrationTest {
-
+    ChildRepository repository;
     @Override
     public void setUp() throws Exception {
         super.setUp();
         loginPage.login();
+        String timeStamp = now().defaultFormat();
+        seed(new Child(String.format("{ '_id' : '123456', 'timeStamp' : '%s', 'test2' : 'value2', 'unique_identifier' : 'abcd1234', 'one' : '1' }", timeStamp)));
+        repository = RapidFtrApplication.getInstance().getInjector().getInstance(ChildRepository.class);
     }
 
-    public void workInProgressRecordIsSuccessfullySyncedAndIdPopulatedWithCouchIdFromServer() throws JSONException, IOException {
-        String timeStamp = now().defaultFormat();
-        seed(new Child(String.format("{ '_id' : '123', 'timeStamp' : '%s', 'test2' : 'value2' }", timeStamp)));
-        solo.clickOnMenuItem("Synchronize");
-        solo.waitForDialogToClose(10000);
-        ChildRepository repository = RapidFtrApplication.getInstance().getInjector().getInstance(ChildRepository.class);
+    public void testRecordIsSuccessfullyDownloadedFromServer() throws JSONException, IOException, InterruptedException {
+        solo.clickOnMenuItem(solo.getString(R.string.synchronize_all));
+        Thread.sleep(5000); //Sleep for synchronization to happen.
 
-        Child retrievedChild = repository.get("123");
-        assertThat(retrievedChild.get("timeStamp").toString(), is(timeStamp));
+
+        assertTrue(repository.exists("abcd1234"));
+        Child child = repository.get("abcd1234");
+        assertEquals("123456", child.optString("_id"));
+    }
+
+    public void testRecordShouldBeUploadedToServer() throws JSONException, InterruptedException {
+
+        Child childToBeSynced = new Child("'unique_identifier' : 'xyz4321','name' : 'moses'");
+        repository.createOrUpdate(childToBeSynced);
+        assertFalse(childToBeSynced.isSynced());
+        solo.clickOnMenuItem(solo.getString(R.string.synchronize_all));
+        Thread.sleep(5000); //Sleep for synchronization to happen.
+        assertTrue(repository.exists("xyz4321"));
+        assertTrue(childToBeSynced.isSynced());
+
     }
 
     private void seed(Child child) throws JSONException, IOException {
