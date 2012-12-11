09f91355c028c80a288510e1e58bd459c691eb82
akash <akash@thoughtworks.com>
2012-12-11 12:26:31 +0300

    #1234 - Akash - forgot to bind new service and added toast message

--- RapidFTR-Android/src/main/java/com/rapidftr/service/LogOutService.java
+++ RapidFTR-Android/src/main/java/com/rapidftr/service/LogOutService.java
@@ -1,7 +1,9 @@
 package com.rapidftr.service;
 
+import android.widget.Toast;
 import com.rapidftr.RapidFtrApplication;
 
+import static android.widget.Toast.LENGTH_LONG;
 import static com.rapidftr.RapidFtrApplication.Preference.USER_NAME;
 import static com.rapidftr.RapidFtrApplication.Preference.USER_ORG;
 
@@ -11,5 +13,6 @@ public class LogOutService {
         context.setLoggedIn(false);
         context.removePreference(USER_NAME);
         context.removePreference(USER_ORG);
+        Toast.makeText(context, "You have been logged out.", LENGTH_LONG).show();
     }
 }
