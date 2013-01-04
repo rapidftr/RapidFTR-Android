9a1c3cd353606237d66c957149acb5f16fb5091e
Yekkanti Kishore Kumar <kishoreyekkanti@gmail.com>
2013-01-05 00:55:38 +0530

    [#1476] Yekkanti - Added new database for unauthorized users

--- RapidFTR-Android/src/main/java/com/rapidftr/utils/ApplicationInjector.java
+++ RapidFTR-Android/src/main/java/com/rapidftr/utils/ApplicationInjector.java
@@ -8,12 +8,14 @@ import com.rapidftr.RapidFtrApplication;
 import com.rapidftr.database.DatabaseHelper;
 import com.rapidftr.database.DatabaseSession;
 import com.rapidftr.database.SQLCipherHelper;
+import com.rapidftr.model.User;
 import com.rapidftr.repository.ChildRepository;
 import com.rapidftr.service.ChildService;
 import com.rapidftr.service.FormService;
 import com.rapidftr.service.LogOutService;
 import com.rapidftr.task.SyncAllDataAsyncTask;
 import com.rapidftr.utils.http.FluentRequest;
+import org.json.JSONException;
 
 import static com.rapidftr.RapidFtrApplication.Preference.SERVER_URL;
 import static com.rapidftr.RapidFtrApplication.Preference.USER_NAME;
@@ -42,10 +44,18 @@ public class ApplicationInjector extends AbstractModule {
         return application.getDbKey();
     }
 
-    @Provides @Named("DB_NAME")
-    public String getDBName(RapidFtrApplication application) {
-        String serverUrl = application.getPreference(SERVER_URL);
-        return serverUrl.replaceAll("^.+//", "").replaceAll("\\W+", "_");
+    @Provides
+    @Named("DB_NAME")
+    public String getDBName(RapidFtrApplication application) throws JSONException {
+        String userJson = application.getPreference(application.getPreference(USER_NAME));
+        User user = new User(userJson);
+        if (!user.isAuthenticated()) {
+            return "UNAUTHENTICATED_STORE";
+        } else {
+            //TODO: The below logic should be changed such that the database name should be generated without considering server URL
+            String serverUrl = application.getPreference(SERVER_URL);
+            return serverUrl.replaceAll("^.+//", "").replaceAll("\\W+", "_");
+        }
     }
 
     @Provides
--- RapidFTR-Android/src/test/java/com/rapidftr/utils/ApplicationInjectorTest.java
+++ RapidFTR-Android/src/test/java/com/rapidftr/utils/ApplicationInjectorTest.java
@@ -6,6 +6,8 @@ import com.google.inject.Key;
 import com.google.inject.name.Names;
 import com.rapidftr.CustomTestRunner;
 import com.rapidftr.RapidFtrApplication;
+import com.rapidftr.model.User;
+import org.json.JSONException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -35,10 +37,23 @@ public class ApplicationInjectorTest {
     }
 
     @Test
-    public void testDbName() {
+    public void testDbNameForAuthenticatedUser() throws JSONException {
+        User user = new User(true, "encrypted-db-key", "org");
         application.setPreference(SERVER_URL, "https://12.34.56.78:90");
+        application.setPreference(USER_NAME, "test");
+        application.setPreference("test", user.toString());
         String result = injector.getInstance(Key.get(String.class, Names.named("DB_NAME")));
         assertThat(result, equalTo("12_34_56_78_90"));
+    }
+
+    @Test
+    public void testDbNameForUnAuthenticatedUser() throws Exception {
+        User user = new User(false, "org", "fullname", "password");
+        application.setPreference(SERVER_URL, "https://12.34.56.78:90");
+        application.setPreference(USER_NAME, "test");
+        application.setPreference("test", user.toString());
+        String result = injector.getInstance(Key.get(String.class, Names.named("DB_NAME")));
+        assertThat(result, equalTo("UNAUTHENTICATED_STORE"));
     }
 
 }
