fc704d51fa261deccfe73036ff2930ae2a398548
Ashok <ashokk@thoughtworks.com>
2013-02-06 12:12:38 +0530

    FIX - Ashok - Adding the unregister receiver in try catch to prevent it braking app if receiver is not registered

--- RapidFTR-Android/src/main/java/com/rapidftr/activity/RapidFtrActivity.java
+++ RapidFTR-Android/src/main/java/com/rapidftr/activity/RapidFtrActivity.java
@@ -195,7 +195,11 @@ public abstract class RapidFtrActivity extends Activity {
     @Override
     protected void onStop(){
         super.onStop();
-        unregisterReceiver(networkChangeReceiver);
+        try{
+            unregisterReceiver(networkChangeReceiver);
+        }catch(IllegalArgumentException e){
+            logError(e.getMessage());
+        }
     }
 
     protected void initializeExceptionHandler() {
