b7ce75ff4a239671a2b57b5c18d53fa1caa0ace4
Subhas Dandapani <subhasda@thoughtworks.com>
2013-04-04 19:08:00 +0530

    1707 | Subhas/Kavitha | Fix session token bug

--- RapidFTR-Android/src/test/java/com/rapidftr/utils/http/FluentRequestTest.java
+++ RapidFTR-Android/src/test/java/com/rapidftr/utils/http/FluentRequestTest.java
@@ -2,6 +2,9 @@ package com.rapidftr.utils.http;
 
 import android.content.Context;
 import com.rapidftr.CustomTestRunner;
+import com.rapidftr.RapidFtrApplication;
+import com.rapidftr.model.User;
+import com.rapidftr.utils.RapidFtrDateTime;
 import com.xtremelabs.robolectric.Robolectric;
 import com.xtremelabs.robolectric.tester.org.apache.http.FakeHttpLayer;
 import com.xtremelabs.robolectric.tester.org.apache.http.RequestMatcher;
@@ -23,6 +26,7 @@ import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.security.Security;
 
+import static com.rapidftr.CustomTestRunner.createUser;
 import static com.rapidftr.utils.http.FluentRequest.http;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
@@ -108,6 +112,26 @@ public class FluentRequestTest {
         Robolectric.getFakeHttpLayer().addHttpResponseRule("http://example.com/test", response);
         assertThat(http.path("/test").context(context).get(), equalTo(response));
     }
+
+	@Test
+	public void testSessionIdFromContext() {
+		User user = createUser();
+		user.setSessionId("test_session_id");
+		RapidFtrApplication.getApplicationInstance().setCurrentUser(user);
+
+		FluentRequest http = spy(http());
+		http.context(RapidFtrApplication.getApplicationInstance());
+
+		verify(http).header("Authorization", "RFTR_Token test_session_id");
+	}
+
+	@Test
+	public void shouldNotSetSessionIdIfNoUser() {
+		RapidFtrApplication.getApplicationInstance().setCurrentUser(null);
+		FluentRequest http = spy(http());
+		http.context(RapidFtrApplication.getApplicationInstance());
+		verify(http).header("Authorization", "");
+	}
 
 	@Test
 	public void testTimeoutFromContext() {
