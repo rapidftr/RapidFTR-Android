eeee215af875be241a3cdd4d0139c080c4487c02
gowrirj <gowrirj@thoughtworkss-MacBook-Pro-3.local>
2013-04-12 18:04:54 +0530

    Gowri:added tests around unverfied users

--- RapidFTR-AndroidTests/src/main/java/com/rapidftr/activity/UnverifiedUsersIntegrationTest.java
+++ RapidFTR-AndroidTests/src/main/java/com/rapidftr/activity/UnverifiedUsersIntegrationTest.java
@@ -3,31 +3,82 @@ package com.rapidftr.activity;
 import android.content.Context;
 import android.net.wifi.WifiManager;
 import com.rapidftr.R;
+import com.rapidftr.RapidFtrApplication;
+import com.rapidftr.activity.pages.LoginPage;
 import com.rapidftr.model.Child;
 import com.rapidftr.repository.ChildRepository;
+import com.rapidftr.test.utils.RapidFTRDatabase;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 public class UnverifiedUsersIntegrationTest extends BaseActivityIntegrationTest{
     boolean wifi=false;
-    Child child;
     String password ="a";
     String organisation="TW";
     ChildRepository repository;
     String childName;
+    List childDetails;
+    Child child;
 
     @Override
     public void setUp() throws Exception{
         super.setUp();
-         childName=getAlphaNumeric(5);
+        childName=getAlphaNumeric(5);
+        unverifiedUserPage.clickSignUpLink();
+        unverifiedUserPage.registerUnverifiedUser(childName, password, password,childName, organisation);
+        solo.sleep(5);
+        loginPage.login(childName, password, loginPage.LOGIN_URL);
+        solo.waitForText("Login Successful");
+        repository = RapidFtrApplication.getApplicationInstance().getInjector().getInstance(ChildRepository.class);
+        RapidFTRDatabase.deleteChildren();
+
     }
 
-    public void testUnverifiedUserCreationAndSyncRecordsToServer(){
-//        turnWifi(wifi);
-        solo.clickOnText(solo.getString(R.string.signup));
-        loginPage.registerUnverifiedUser(childName, password, password, childName, organisation);
-        loginPage.login(childName,password,loginPage.LOGIN_URL);
-        assertTrue(solo.waitForText("Login Successful"));
+    @Override
+    public void tearDown() throws Exception{
+        repository.close();
+        super.tearDown();
     }
 
+    public void testUnverifiedUserCreationErrorMessages(){
+        loginPage.logout();
+        unverifiedUserPage.clickSignUpLink();
+        unverifiedUserPage.registerUnverifiedUser(" "," "," "," "," ");
+        //try hash map here
+        List<String> expectedErrorMessage = new ArrayList<String>(asList(new String[]{"Username is required","Password is required","Re-type password",
+                "Full name required","Organisation is required"}));
+        List<String> actualErrorMessage=unverifiedUserPage.getFieldsErrorMessages();
+        assertTrue(expectedErrorMessage.equals(actualErrorMessage));
+    }
+
+    public void testUnverifiedUserAbleToSyncRecordsToServer() throws JSONException,InterruptedException {
+        childPage.navigateToRegisterPage();
+        childPage.selectFormSection("Automation Form");
+        childPage.registerChild();
+        viewAllChildrenPage.navigateToViewAllTab();
+        childDetails = repository.getChildrenByOwner();
+        child=new Child(childDetails.get(0).toString());
+        solo.clickOnMenuItem(solo.getString(R.string.synchronize_all));
+        assertTrue(unverifiedUserPage.verifySyncLocationPopUp());
+        unverifiedUserPage.enterSyncLocation(LoginPage.LOGIN_URL);
+        assertTrue(repository.exists(child.getUniqueId()));
+        List<Child> children=repository.getMatchingChildren(child.getUniqueId());
+        assertEquals(1,children.size());
+    }
+
+    public void testUnverifiedUserUnableToViewVerifiedUserRecords() throws JSONException,InterruptedException{
+        child= new Child(getAlphaNumeric(5), "admin", "{'name' : 'adminuser'}");
+        repository.createOrUpdate(child);
+        viewAllChildrenPage.navigateToViewAllTab();
+        solo.clickOnMenuItem(solo.getString(R.string.synchronize_all));
+        unverifiedUserPage.enterSyncLocation(LoginPage.LOGIN_URL);
+        assertFalse(searchPage.isChildPresent(child.getUniqueId(),child.getName()));
+
+    }
     public void estUnverifiedUserSyncProcessAfterMakingAsVerifiedUser(){
 
     }
