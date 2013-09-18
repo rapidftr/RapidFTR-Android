5ff91ebe1dc5ba153d98f304623f27ed5ca90627
Sri Prasanna <k.sriprasanna@gmail.com>
2013-09-18 16:19:32 +0300

    #1859 + @Machaba-Machaba - Refactoring and added new class to have common components between collection

--- RapidFTR-Android/src/main/java/com/rapidftr/activity/BaseEnquiryActivity.java
+++ RapidFTR-Android/src/main/java/com/rapidftr/activity/BaseEnquiryActivity.java
@@ -11,6 +11,7 @@ import com.google.common.io.CharStreams;
 import com.rapidftr.R;
 import com.rapidftr.adapter.FormSectionPagerAdapter;
 import com.rapidftr.forms.FormSection;
+import com.rapidftr.model.BaseModel;
 import com.rapidftr.model.Enquiry;
 import com.rapidftr.repository.EnquiryRepository;
 import com.rapidftr.task.AsyncTaskWithDialog;
@@ -24,10 +25,19 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
 
-public abstract class BaseEnquiryActivity extends RapidFtrActivity {
+public abstract class BaseEnquiryActivity extends CollectionActivity {
     protected Enquiry enquiry;
-    protected List<FormSection> formSections;
     public static final ObjectMapper JSON_MAPPER = new ObjectMapper();
+
+    @Override
+    protected Boolean getEditable() {
+        return true;
+    }
+
+    @Override
+    protected BaseModel getModel() {
+        return enquiry;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,38 +63,6 @@ public abstract class BaseEnquiryActivity extends RapidFtrActivity {
         @Cleanup InputStream in = getResources().openRawResource(R.raw.enquiry_form_sections);
         String x = CharStreams.toString(new InputStreamReader(in));
         formSections = Arrays.asList(JSON_MAPPER.readValue(x, FormSection[].class));
-    }
-
-    protected Spinner getSpinner() {
-        return ((Spinner) findViewById(R.id.spinner));
-    }
-
-    protected ViewPager getPager() {
-        return (ViewPager) findViewById(R.id.pager);
-    }
-
-    protected void initializePager() {
-        getPager().setAdapter(new FormSectionPagerAdapter(formSections, enquiry, true));
-        getPager().setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                getSpinner().setSelection(position);
-            }
-
-        });
-    }
-
-    protected void initializeSpinner() {
-        getSpinner().setAdapter(new ArrayAdapter<FormSection>(this, android.R.layout.simple_spinner_item, formSections));
-        getSpinner().setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                getPager().setCurrentItem(position);
-            }
-
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
     }
 
 
---
+++ RapidFTR-Android/src/main/java/com/rapidftr/activity/CollectionActivity.java
@@ -0,0 +1,52 @@
+package com.rapidftr.activity;
+
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import com.rapidftr.R;
+import com.rapidftr.adapter.FormSectionPagerAdapter;
+import com.rapidftr.forms.FormSection;
+import com.rapidftr.model.BaseModel;
+
+import java.util.List;
+
+public abstract class CollectionActivity extends RapidFtrActivity {
+    protected List<FormSection> formSections;
+
+    protected abstract BaseModel getModel();
+    protected abstract Boolean getEditable();
+
+    protected Spinner getSpinner() {
+        return ((Spinner) findViewById(R.id.spinner));
+    }
+
+    protected ViewPager getPager() {
+        return (ViewPager) findViewById(R.id.pager);
+    }
+
+    protected void initializePager() {
+        getPager().setAdapter(new FormSectionPagerAdapter(formSections, getModel(), getEditable()));
+        getPager().setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                getSpinner().setSelection(position);
+            }
+
+        });
+    }
+
+    protected void initializeSpinner() {
+        getSpinner().setAdapter(new ArrayAdapter<FormSection>(this, android.R.layout.simple_spinner_item, formSections));
+        getSpinner().setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                getPager().setCurrentItem(position);
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+}
