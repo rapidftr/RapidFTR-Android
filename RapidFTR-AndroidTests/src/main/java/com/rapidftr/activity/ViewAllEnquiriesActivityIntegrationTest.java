5c11b614edb97efef293ba4c2c5890d82e3b81c4
machaba <machaba@thoughtworks.com>
2013-09-25 11:26:31 +0300

    Removed duplicated integration tests

--- RapidFTR-AndroidTests/src/main/java/com/rapidftr/activity/ViewAllEnquiriesActivityIntegrationTest.java
+++ RapidFTR-AndroidTests/src/main/java/com/rapidftr/activity/ViewAllEnquiriesActivityIntegrationTest.java
@@ -1,9 +1,7 @@
 package com.rapidftr.activity;
 
 import com.rapidftr.RapidFtrApplication;
-import com.rapidftr.model.Child;
 import com.rapidftr.model.Enquiry;
-import com.rapidftr.repository.ChildRepository;
 import com.rapidftr.repository.EnquiryRepository;
 import junit.framework.Assert;
 import org.json.JSONException;
@@ -29,21 +27,6 @@ public class ViewAllEnquiriesActivityIntegrationTest extends BaseActivityIntegra
         assertTrue(viewAllEnquiriesPage.isEnquiryPresent(enquiry1));
         assertTrue(viewAllEnquiriesPage.isEnquiryPresent(enquiry2));
     }
-
-    public void testClickOnChildShouldShowViewPage() throws JSONException {
-
-        ChildRepository repository = RapidFtrApplication.getApplicationInstance().getInjector().getInstance(ChildRepository.class);
-        Child child1 = new Child(getAlphaNumeric(4), "admin", "{\"name\":\"Test1\"}");
-        repository.createOrUpdate(child1);
-        Child child2 = new Child(getAlphaNumeric(6), "admin", "{\"name\":\"Test2\"}");
-        repository.createOrUpdate(child2);
-        viewAllChildrenPage.navigateToViewAllTab();
-        viewAllChildrenPage.clickChild(child1.getUniqueId());
-        viewAllChildrenPage.verifyChildDetails(child1);
-    }
-
-
-
 
 
 }
