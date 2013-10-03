7f09cb8d6b141c60f783d4f5fb29ac3ea8d54592
Faris Mohammed <farismosman@gmail.com>
2013-10-03 15:39:27 +0300

    [#1866] Godwin/Faris - Added potential matches form section to the view.

--- RapidFTR-Android/src/main/java/com/rapidftr/activity/CreateEnquiryActivity.java
+++ RapidFTR-Android/src/main/java/com/rapidftr/activity/CreateEnquiryActivity.java
@@ -1,7 +1,19 @@
 package com.rapidftr.activity;
 
+import android.os.Bundle;
 import android.view.View;
+import com.google.common.io.CharStreams;
 import com.rapidftr.R;
+import com.rapidftr.forms.FormSection;
+import com.rapidftr.model.Enquiry;
+import lombok.Cleanup;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CreateEnquiryActivity extends BaseEnquiryActivity {
     public void initializeView() {
@@ -12,5 +24,16 @@ public class CreateEnquiryActivity extends BaseEnquiryActivity {
                 save();
             }
         });
+    }
+    
+    @Override
+    protected void initializeData(Bundle savedInstanceState) throws JSONException, IOException {
+        enquiry = new Enquiry();
+        @Cleanup InputStream in = getResources().openRawResource(R.raw.enquiry_form_sections);
+        String x = CharStreams.toString(new InputStreamReader(in));
+        formSections = new ArrayList<FormSection>(Arrays.asList(JSON_MAPPER.readValue(x, FormSection[].class)));
+        int indexOfPotentialMatches = formSections.size() - 1;
+
+        formSections.remove(indexOfPotentialMatches);
     }
 }
--- RapidFTR-Android/src/test/java/com/rapidftr/activity/CreateEnquiryActivityTest.java
+++ RapidFTR-Android/src/test/java/com/rapidftr/activity/CreateEnquiryActivityTest.java
@@ -3,11 +3,12 @@ package com.rapidftr.activity;
 import com.rapidftr.CustomTestRunner;
 import com.rapidftr.R;
 import com.rapidftr.model.Enquiry;
-import org.json.JSONException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
@@ -33,4 +34,9 @@ public class CreateEnquiryActivityTest {
         assertEquals(activity.enquiry.getClass(), Enquiry.class);
     }
 
+    @Test
+    public void testRemovePotentialMatchesSectionWhenExist() throws Exception {
+        activity.initializeData(null);
+        assertThat(activity.formSections.size(), is(6));
+    }
 }
