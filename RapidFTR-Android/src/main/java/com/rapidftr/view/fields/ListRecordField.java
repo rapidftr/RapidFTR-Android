631fd1fc06bce10ad386b742a8d583b8d85a0eb6
gsemwezi <gsemwezi@gmail.com>
2013-10-17 13:47:06 +0300

    Added listview ui for the potential matches

--- RapidFTR-Android/src/main/java/com/rapidftr/view/fields/ListRecordField.java
+++ RapidFTR-Android/src/main/java/com/rapidftr/view/fields/ListRecordField.java
@@ -2,6 +2,9 @@ package com.rapidftr.view.fields;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
@@ -44,6 +47,29 @@ public class ListRecordField extends BaseView {
             childListView.setEmptyView(findViewById(R.id.no_matches));
         } else
         childListView.setAdapter(childViewAdapter);
+        setListViewHeightBasedOnChildren(childListView);
+    }
+
+    private static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            return;
+        }
+        int desiredWidth = MeasureSpec.makeMeasureSpec(listView.getWidth(), MeasureSpec.AT_MOST);
+        int totalHeight = 0;
+        View view = null;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            view = listAdapter.getView(i, view, listView);
+            if (i == 0) {
+                view.setLayoutParams(new ViewGroup.LayoutParams(desiredWidth, LayoutParams.WRAP_CONTENT));
+            }
+            view.measure(desiredWidth, MeasureSpec.UNSPECIFIED);
+            totalHeight += view.getMeasuredHeight();
+        }
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
     }
 
     @Override
