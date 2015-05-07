f18c6a93cd4ddbff90983ed1e75d25f897fb61a1
Tom Oketch <tc.oketch@gmail.com>
2015-05-07 13:15:31 +0300

    Ensure emulator tests run on SnapCI

--- RapidFTR-Android/travis/emulator_test.sh
+++ RapidFTR-Android/travis/emulator_test.sh
@@ -7,7 +7,8 @@ emulator -avd test -no-skin -no-audio -no-window -no-boot-anim &
 
 mvn clean package -DskipTests -P calabash
 bundle install
+bundle install -j4 --binstubs
 
 ./travis/wait_for_emulator.sh
 adb shell input keyevent 82
-calabash-android run `ls target/*.apk | head -1` -f rerun --out rerun.txt -f pretty --tags ~@ignore
+bundle exec calabash-android run `ls target/*.apk | head -1` -f rerun --out rerun.txt -f pretty --tags ~@ignore
