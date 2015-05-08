d9f380df1a9212d724ccdb9eb9f819b3e8238868
Tom Oketch <tc.oketch@gmail.com>
2015-05-08 11:58:32 +0300

    @tomclement - Run emulator tests in parallel on SnapCI

--- RapidFTR-Android/travis/emulator_test.sh
+++ RapidFTR-Android/travis/emulator_test.sh
@@ -11,4 +11,18 @@ bundle install -j4 --binstubs
 
 ./travis/wait_for_emulator.sh
 adb shell input keyevent 82
-bundle exec calabash-android run `ls target/*.apk | head -1` -f rerun --out rerun.txt -f pretty --tags ~@ignore
+
+if [ ! -n "$1" ]
+    then
+    echo $'\e[32m'"Running all features"$'\e[0m'
+    bundle exec calabash-android run `ls target/*.apk | head -1` -f rerun --out rerun.txt -f pretty --tags ~@ignore
+
+    else
+    echo $'\e[32m'"Running only the following features:"$'\e[0m'
+    for feature in "$@"
+        do
+        echo $'\e[32m'" [x] $feature"$'\e[0m'
+        done
+        echo ""
+    bundle exec calabash-android run `ls target/*.apk | head -1` -f rerun --out rerun.txt -f pretty --tags ~@ignore $@
+    fi
---
+++ RapidFTR-Android/travis/emulator_test_snapci.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if (( ${SNAP_WORKER_TOTAL:-0} < 2 )); then
+  echo "Not enough workers to run tests."
+  exit -1
+fi
+
+case "$SNAP_WORKER_INDEX" in
+  1) ./emulator_test.sh features/login.feature features/navigation.feature features/register.feature ;;
+  2) ./emulator_test.sh features/search.feature features/sync.feature features/unverified_user.feature ;;
+esac
