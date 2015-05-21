54e84737f62b27fc6c3608a741d90e7a945df96b
Tom Oketch <tc.oketch@gmail.com>
2015-05-21 17:32:10 +0300

    Make sure APKs are signed with specific keystore when building

--- RapidFTR-Android/travis/release_apk.sh
+++ RapidFTR-Android/travis/release_apk.sh
@@ -6,11 +6,11 @@ echo $DROPBOX_CFG | sed 's/\\n/\'$'\n/g' > dropbox_config
 
 if [ "$1" = "rapidreg" ]
     then
-    (cd RapidFTR-Android && mvn clean package -DskipTests -Dbrand=rapidreg)
+    (cd RapidFTR-Android && mvn clean package -DskipTests -Dbrand=rapidreg -Psign -Dkeystore.file=../rapidftr.keystore -Dkey.password=$RAPIDFTR_KEY_PASSWORD -Dkeystore.password=$RAPIDFTR_KEYSTORE_PASSWORD -U)
     ./dropbox_uploader.sh -f dropbox_config upload `ls RapidFTR-Android/target/rapidftr-*.apk | head -1` RapidREG/Android/RapidREG-dev.apk
 
     else
-    (cd RapidFTR-Android && mvn clean package -DskipTests)
+    (cd RapidFTR-Android && mvn clean package -DskipTests -Psign -Dkeystore.file=../rapidftr.keystore -Dkey.password=$RAPIDFTR_KEY_PASSWORD -Dkeystore.password=$RAPIDFTR_KEYSTORE_PASSWORD -U)
     ./dropbox_uploader.sh -f dropbox_config upload `ls RapidFTR-Android/target/rapidftr-*.apk | head -1` Releases/Android/RapidFTR-dev.apk
 
     fi
